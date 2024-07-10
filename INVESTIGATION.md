be9b725c787a629bfc08d609ccbd83198e5818d8
Andrei Voicu <Andrei.Voicu@softwire.com>
2024-07-10 15:17:47 +0300

    completed task2

--- INVESTIGATION.md
+++ INVESTIGATION.md
@@ -1,22 +1,39 @@
-## dataset_info - discovering pandas functionalities
-# dataframe.info()
+# Part 1
+## dataframe.info()
     - prints a table containing the name of the columns
     - the number of rows containing non null values for the given column
     - the type of data found in that column
-# dataframe.isnull()
+## dataframe.isnull()
     - prints the entire csv, with "False" instead of non null values or "True" instead of null values
     - prefered to use as ".isnull().sum()" because it shows the number of non null values for each column
-# dataframe.duplicated()
+## dataframe.duplicated()
     - returns a boolean series where duplicated rows are marked with "True"
     - prefered to use as ".duplicated().sum()" because it shows the number of duplicated rows.
-# dataframe.min()
+## dataframe.min()
     - minimum of a row -> min(axis = 1)
     - minimum of a column -> min(axis = 0)
     - for columns: returns a series containing the minimum value for each column
     - minimum for a specific column can be found by accessing with series['column_name'], like a python dict
-# dataframe["given_column"].value_counts()
+## dataframe["given_column"].value_counts()
     - returns a series with the number of occurences of each value found in "given_column"
-# series.iloc[index]
+## series.iloc[index]
     - used for integer-based indexing of a series. Useful for series with labels of types other than integer
-# series.index
+## series.index
     - returns a list of indexes
+
+# Part 2
+## sns.hisplot()
+    - takes as parameters the dataframe and the name of the column that should be plotted on the x-axis
+    - need to call plt.show() to print the plot
+![img_1.png](img_1.png)
+## sns.FacetGrid() - max temperature
+    - it is initialized with the dataframe and the column
+    - used lineplot
+![img_2.png](img_2.png)
+## sns.FacetGrid() - precipitation
+    - used scatterplot
+![img.png](img.png)
+## sns.countplot() - weather
+![img_3.png](img_3.png)
+## plt.pie - weather piechart
+![img_4.png](img_4.png)
--- main.py
+++ main.py
@@ -1,59 +1,81 @@
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 
-def dataset_info(dataframe: pd.DataFrame):
+def dataset_info(df: pd.DataFrame):
     """ TODO:
     """
     # printing info about dataframe
     print("dataframe info")
-    dataframe.info()
+    df.info()
 
     # finding the number of null values in entire dataframe
     print("\nnumber of null values")
-    print(dataframe.isnull().sum())
+    print(df.isnull().sum())
 
     # finding the number of duplicated rows in entire dataframe
     print("\nnumber of duplicated rows")
-    print(dataframe.duplicated().sum())
+    print(df.duplicated().sum())
 
     # finding the minimum temperature
     print("\nminimum temperature")
-    print(dataframe.min(axis=0)["temp_min"])
+    print(df.min(axis=0)["temp_min"])
 
     # finding the maximum temperature
     print("\nmaximum temperature")
-    print(dataframe.max(axis=0)["temp_max"])
+    print(df.max(axis=0)["temp_max"])
 
     # most common weather condition
     print("\nmost common weather condition")
-    weather_values_count = dataframe["weather"].value_counts()
+    weather_values_count = df["weather"].value_counts()
     weather_values_count.sort_values(ascending=False)
     print(weather_values_count.index[0])
 
 
-def temp_max_histplot(dataframe: pd.DataFrame):
+def temp_max_histplot(df: pd.DataFrame):
     """ TODO:
     """
+    sns.histplot(data=df, x="temp_max")
+    plt.show()
 
 
-def temp_max_facegrid_lineplot(dataframe: pd.DataFrame):
+def temp_max_facegrid_lineplot(df: pd.DataFrame):
     """ TODO:
     """
+    df["date"] = pd.to_datetime(df["date"])
+    df["year"] = df["date"].dt.year
+    df["month"] = df["date"].dt.month
 
+    grid = sns.FacetGrid(df, col="year")
+    grid.map(sns.lineplot, "month", "temp_max")
+    plt.show()
 
-def precipitation_facegrid_scatterplot(dataframe: pd.DataFrame):
+def precipitation_facegrid_scatterplot(df: pd.DataFrame):
     """ TODO:
     """
+    df["date"] = pd.to_datetime(df["date"])
+    df["year"] = df["date"].dt.year
+    df["month"] = df["date"].dt.month
 
+    grid = sns.FacetGrid(df, col="year")
+    grid.map(sns.scatterplot, "month", "precipitation")
+    plt.show()
 
-def weather_countplot(dataframe: pd.DataFrame):
+def weather_countplot(df: pd.DataFrame):
     """ TODO:
     """
+    sns.countplot(data=df, x="weather")
+    plt.show()
 
 
-def weather_piechart(dataframe: pd.DataFrame):
+def weather_piechart(df: pd.DataFrame):
     """ TODO:
     """
+    palette_color = sns.color_palette('bright')
+    weather_count = df["weather"].value_counts()
+    plt.pie(weather_count, labels=weather_count.index, colors=palette_color)
+    plt.show()
 
 
 def lr_predictor_random_split(dataframe: pd.DataFrame):
@@ -73,8 +95,12 @@ def svr_predictor_default_split(dataframe: pd.DataFrame):
 
 def main():
     df = pd.read_csv('seattle-weather.csv')
-    dataset_info(df)
-
+    # dataset_info(df)
+    # temp_max_histplot(df)
+    # temp_max_facegrid_lineplot(df)
+    # precipitation_facegrid_scatterplot(df)
+    # weather_countplot(df)
+    weather_piechart(df)
 
 if __name__ == '__main__':
     main()
